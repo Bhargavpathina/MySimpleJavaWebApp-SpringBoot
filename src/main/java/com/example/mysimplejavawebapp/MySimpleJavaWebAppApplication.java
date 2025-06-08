d23c6b79275ff2798fac9b91998c478615778384
Bhargavpathina <universev456@gmail.com>
2025-06-08 12:07:47 +0530

    Update MySimpleJavaWebAppApplication.java

--- src/main/java/com/example/mysimplejavawebapp/MySimpleJavaWebAppApplication.java
+++ src/main/java/com/example/mysimplejavawebapp/MySimpleJavaWebAppApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
 
 @SpringBootApplication
 public class MySimpleJavaWebAppApplication extends SpringBootServletInitializer {
@@ -17,3 +22,12 @@ public class MySimpleJavaWebAppApplication extends SpringBootServletInitializer 
         return application.sources(MySimpleJavaWebAppApplication.class);
     }
 }
+@RestController
+        public class HelloWorldController {
+
+            @GetMapping("/")
+            public String hello() {
+                return "Hello from Azure DevOps Java App from GitHub!";
+            }
+        }
+        
